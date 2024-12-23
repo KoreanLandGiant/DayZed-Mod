ab674a3f5a357b879e7daf2ead60970976094531
KoreanLandGiant <108334121+KoreanLandGiant@users.noreply.github.com>
2024-12-22 18:56:06 -0800

    mod files

---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name = "DayZed"; // name
+picture = ""; // picture in expanded description
+logoSmall = ""; // icon next to mod name when description is not expanded
+logo = ""; // logo below game menu
+logoOver = ""; // on mouse hover over logo
+tooltip = "DayZed"; // tool tip on mouse hover
+overview = "DayZed"; // overview
+action = "https://DayZed.gg/"; // link
+author = "DayZed"; // author
+version = "0.80"; // version
